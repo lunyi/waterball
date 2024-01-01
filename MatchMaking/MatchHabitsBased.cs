31669b8ca51016edb8bcecbc092bfe420a9b1c26
lester.wei <lunyi.stock@gmail.com>
2024-01-01 19:01:40 +0800

    fix minor fix

--- MatchMaking/MatchHabitsBased.cs
+++ MatchMaking/MatchHabitsBased.cs
@@ -5,9 +5,7 @@
         public Individual[] Match<T>(Individual person, Individual[] individuals, Func<Dictionary<Individual, T>, IOrderedEnumerable<KeyValuePair<Individual, T>>> orderby) where T : struct
         {
             individuals = individuals.Where(p => p.Id != person.Id).ToArray();
-
             var habits = person.Habits.Split(',');
-
             var inds = new Dictionary<Individual, T>();
 
             for (int i = 0; i < individuals.Length; i++)
@@ -16,7 +14,6 @@
                 var indHabits = individuals[i].Habits.Split(',');
                 for (int j = 0; j < indHabits.Length; j++)
                 {
-                    
                     for (int k = 0; k < habits.Length; k++)
                     {
                         if (indHabits[j].Contains(habits[k]))
@@ -27,7 +24,6 @@
                 }
                 inds.Add(individuals[i], (T)(object)habitCount);
             }
-
             return orderby(inds).Select(p => p.Key).ToArray();
         }
     }
